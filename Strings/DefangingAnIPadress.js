6b4871faa153dd73775a00f39d111f84c6629967
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-12-10 11:09:15 +0530

    done on Leetcode

---
+++ Strings/DefangingAnIPadress.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+// var defangIPaddr = function (address) {
+//   // we can simply use a function
+//   return address.replaceAll(".", "[.]");
+// };
+
+var defangIPaddr1 = function (address) {
+  let result = "";
+  for (let i = 0; i < address.length; i++) {
+    if (address[i] == ".") {
+      result = result + "[.]";
+    } else {
+      result = result + address[i];
+    }
+  }
+  return result;
+};
+console.log(defangIPaddr1("1.1.1.1."));
