1612092c331f5f47d6d3f680b156736c2c63fafc
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-12-11 09:53:00 +0530

    done on Leetcode

---
+++ Arrays/ElemAppearMoreThan25Perc.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function (arr) {
+  let count = 1;
+  if (arr.length == 1) {
+    return arr[0];
+  }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] != arr[i - 1]) {
+      count = 0;
+    }
+    count++;
+    if (count > arr.length / 4) {
+      return arr[i];
+    }
+  }
+};
