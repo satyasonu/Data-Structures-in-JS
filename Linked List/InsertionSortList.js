d9b4f93f5a92447bca10f80d12c9ac0afda11454
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-12-25 06:03:45 +0530

    done on Leetcode

--- Linked List/InsertionSortList.js
+++ Linked List/InsertionSortList.js
@@ -10,5 +10,21 @@
  * @return {ListNode}
  */
 var insertionSortList = function (head) {
+  let newHead = new ListNode(0);
   if (!head) return head;
+  let prev = head;
+  let curr = head.next;
+  while (curr) {
+    if (curr.val >= prev.val) {
+      prev = curr;
+      curr = curr.next;
+    } else {
+      //   let temp = newNode;
+      let temp = curr;
+      curr = prev;
+      prev = temp;
+      break;
+    }
+  }
+  return curr.next;
 };
