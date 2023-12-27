5bbf115999fa8d3657f70c6e9ed5c7f1832c799a
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-12-27 18:35:24 +0530

    done on leetcode

--- Dynamic Programming/MinimumTimeToMakeRopeColourfull.js
+++ Dynamic Programming/MinimumTimeToMakeRopeColourfull.js
@@ -3,4 +3,22 @@
  * @param {number[]} neededTime
  * @return {number}
  */
-var minCost = function (colors, neededTime) {};
+var minCost = function (colors, neededTime) {
+  let time = 0;
+  for (var i = 0; i < colors.length - 1; i++) {
+    if (colors[i] != colors[i + 1]) {
+      continue;
+    } else {
+      let ballonColor = colors[i];
+      let maxBallon = 1 << 31;
+      let totalTime = 0;
+      while (i < colors.length && colors[i] == ballonColor) {
+        totalTime += neededTime[i];
+        maxBallon = Math.max(maxBallon, neededTime[i]);
+        i++;
+      }
+      time = time + (totalTime - maxBallon);
+    }
+  }
+  return time;
+};
