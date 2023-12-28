cac1eec9f3a759e06bf2817f899cfdd69249126e
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-12-28 09:30:11 +0530

    done

---
+++ Arrays/LargestElementInAnArray.js
@@ -0,0 +1,10 @@
+let array = [3, 2, 1, 5, 2];
+let largest = array[0];
+for (let i = 1; i < array.length; i++) {
+  if (largest > array[i]) {
+    continue;
+  } else {
+    largest = array[i];
+  }
+  console.log(largest);
+}
