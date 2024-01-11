e4669ffe2bdc4cccd4a268015bb5ce0b8a8d3e80
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-11 14:27:12 +0530

    done on Leetcode

---
+++ Arrays/BasicsOfAnArray.js
@@ -0,0 +1,13 @@
+const cars = ["volvo", "alto", "BMW"];
+console.log(cars[1]);
+
+const fruits = new Array(5);
+fruits[0] = "Apple";
+fruits[2] = "Mango";
+console.log(fruits);
+
+// fruits[2] = "kiwi";
+// console.log(fruits[2]);
+
+// const Arr = new Array(5);
+// console.log(Arr);
