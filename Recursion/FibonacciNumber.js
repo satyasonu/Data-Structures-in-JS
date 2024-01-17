56d147864a914e20ee7f4be19fa653eb9a77ebc5
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-17 23:31:21 +0530

    done on Leetcode

--- Recursion/FibonacciNumber.js
+++ Recursion/FibonacciNumber.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var fib = function (n) {
-    if(n == 0){
-        return 0;
-    }else if(n==1){
-        return 1;
-    }
-    fib(n) = fib(n-1) + fib(n-2);
-    return fib(n);
+  var a = [];
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  }
+  a[n] = fib(n - 1) + fib(n - 2);
+  return a[n];
 };
