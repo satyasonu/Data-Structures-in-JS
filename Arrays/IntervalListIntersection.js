c45c2c45c37f2a165e896a7e5a4233a6fd5ef54a
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-08 06:50:04 +0530

    done on leetcode

--- Arrays/IntervalListIntersection.js
+++ Arrays/IntervalListIntersection.js
@@ -4,17 +4,17 @@
  * @return {number[][]}
  */
 var intervalIntersection = function (firstList, secondList) {
-  const result = [];
+  let result = [];
   var i = 0;
   var j = 0;
   while (i < firstList.length && j < secondList.length) {
     let first = firstList[i];
     let second = secondList[j];
 
-    const start = Math.max(first[0], second[0]);
-    const end = Math.min(first[1], second[1]);
-    if (start < end) {
-      result.push[(start, end)];
+    let start = Math.max(first[0], second[0]);
+    let end = Math.min(first[1], second[1]);
+    if (start <= end) {
+      result.push([start, end]);
     }
     if (first[1] < second[1]) {
       i++;
