dcf27a2b9dbb0ce6f4d10142d07f485ffe02323d
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-08 10:48:58 +0530

    done on leetcode

--- Dynamic Programming/PerfectSquare.js
+++ Dynamic Programming/PerfectSquare.js
@@ -3,11 +3,15 @@
  * @return {number}
  */
 var numSquares = function (n) {
-  let dp = new Array(n + 1).fill(Integer.Max_Value);
+  let dp = new Array(n + 1).fill(Number.MAX_VALUE);
   dp[0] = 0;
-  let ans = Integer.Max_Value;
-  for (var i = 1; i * i <= n; i++) {
-    ans = Math.min(ans, 1 + dp[n - i * i]);
+
+  for (let i = 1; i <= n; ++i) {
+    let min_val = Number.MAX_VALUE;
+    for (let j = 1; j * j <= i; ++j) {
+      min_val = Math.min(min_val, dp[i - j * j] + 1);
+    }
+    dp[i] = min_val;
   }
-  return dp[ans];
+  return dp[n];
 };
